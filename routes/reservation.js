772e9245324adebceae2605ec9a11ed767ed2b1e
cent10 <cent10@ajou.ac.kr>
2017-11-30 17:32:23 +0900

    modify reservation page

--- routes/reservation.js
+++ routes/reservation.js
@@ -6,8 +6,8 @@ var dbconf = require('../configs/db_conf.js');
 var connection = new mysql(dbconf);
 
 router.get('/', function(req, res, next){
-    res.send('reservation page');
-    //res.render('html file name');
+    //res.send('reservation page');
+    res.render('reservation_details');
 });
 
 
