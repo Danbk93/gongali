54d4b435b155a3e4f7eb1f46f63fad728df10a00
mjin <myeongjin1220@gmail.com>
2017-11-30 22:01:50 +0900

    ...

--- routes/reservation.js
+++ routes/reservation.js
@@ -5,10 +5,6 @@ var mysql = require('sync-mysql');
 var dbconf = require('../configs/db_conf.js');
 var connection = new mysql(dbconf);
 
-router.get('/', function(req, res, next){
-    res.send('reservation page');
-    //res.render('html file name');
-});
 
 
 
