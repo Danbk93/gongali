6286b9fe43d9adc40afcf095356e5073b7acc1cf
cent10 <cent10@ajou.ac.kr>
2017-11-30 17:36:51 +0900

    rename html file

--- routes/search.js
+++ routes/search.js
@@ -6,8 +6,8 @@ var dbconf = require('../configs/db_conf.js');
 var connection = new mysql(dbconf);
 
 router.get('/', function(req, res, next){
-    res.send('search select page');
-    //res.render('html file name');
+    //res.send('search select page');
+    res.render('facility_search');
 });
 
 router.get('/location', function(req, res, next){
