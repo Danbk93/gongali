161a40f58a4e788f7bbf032505f6eb3e0de2bf68
mjin <myeongjin1220@gmail.com>
2017-12-08 21:20:22 +0900

    ...

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next){
+    if (!req.session.uid) {
+        res.send('<script>alert("로그인 세션이 만료되었습니다. 로그인 화면으로 이동합니다."); location.replace(location.origin);</script>');
+        return;
+    }
     res.render('main_menu', {"username" : req.session.uid});
 });
 
