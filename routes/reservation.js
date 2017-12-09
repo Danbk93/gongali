640d92467abf8c9259696dd80b86efd9012e205b
mjin <myeongjin1220@gmail.com>
2017-12-09 15:14:49 +0900

    update

--- routes/reservation.js
+++ routes/reservation.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next){
+router.get('/list', function(req, res, next){
     if (!req.session.uid) {
         res.send('<script>alert("로그인 세션이 만료되었습니다. 로그인 화면으로 이동합니다."); location.replace(location.origin);</script>');
         return;
@@ -9,6 +9,12 @@ router.get('/', function(req, res, next){
     res.render('reservation_list', {"username" : req.session.uid});
 });
 
-
+router.get('/', function(req, res, next){
+    if (!req.session.uid) {
+        res.send('<script>alert("로그인 세션이 만료되었습니다. 로그인 화면으로 이동합니다."); location.replace(location.origin);</script>');
+        return;
+    }
+    res.render('make_reservation', {"username" : req.session.uid});
+});
 
 module.exports = router;
