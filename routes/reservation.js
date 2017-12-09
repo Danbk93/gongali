6d81820c3d19b9010aac464403e89f474e8aad11
mjin <myeongjin1220@gmail.com>
2017-12-10 02:02:59 +0900

    reservation 수정

--- routes/reservation.js
+++ routes/reservation.js
@@ -1,15 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/list', function(req, res, next){
+var db = require('./db_handler.js');
+
+router.get('/list', function (req, res, next) {
     if (!req.session.uid) {
         res.send('<script>alert("로그인 세션이 만료되었습니다. 로그인 화면으로 이동합니다."); location.replace(location.origin);</script>');
         return;
     }
-    res.render('reservation_list', {"username" : req.session.uid});
+    res.render('reservation_list', {
+        "username": req.session.uid
+    });
 });
 
-router.get('/', function(req, res, next){
+router.get('/', function (req, res, next) {
     if (!req.session.uid) {
         res.send('<script>alert("로그인 세션이 만료되었습니다. 로그인 화면으로 이동합니다."); opener.location.href = location.origin; window.close();</script>');
         return;
@@ -20,30 +24,30 @@ router.get('/', function(req, res, next){
 
 router.post('/available_reservation', function (req, res) {
     var result = req.body;
-  
+
     var facilityID = result.FID
     var reserve_date = result.date
-  
-    console.log(JSON.stringify(result));
+    //reserve_date = reserve_date.substring(0,4) + "-" + reserve_date.substring(4,6) + "-" + reserve_date.substring(6)
+    var rows = db.query("SELECT reservation_number, start_reservation_time, end_reservation_time FROM RESERVATION WHERE FID = " + facilityID + " AND reservation_date = " + reserve_date);
 
-    var rows =  db.query("SELECT reservation_number, start_reservation_time, end_reservation_time FROM RESERVATION WHERE FID = \'" + facilityID + "\' AND reservation_date = \'" + reserve_date + "\'");
-  
     var body = new Object();
     body.result = true;
     body.data = rows;
-  
-    res.send(body);
-  });
+
+    res.json(body);
+});
 
 
 
 
-router.get('/write_review', function(req, res, next){
+router.get('/write_review', function (req, res, next) {
     if (!req.session.uid) {
         res.send('<script>alert("로그인 세션이 만료되었습니다. 로그인 화면으로 이동합니다."); location.replace(location.origin);</script>');
         return;
     }
-    res.render('write_review', {"username" : req.session.uid});
+    res.render('write_review', {
+        "username": req.session.uid
+    });
 });
 
 module.exports = router;
